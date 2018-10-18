3c4b2dccf220967e38e22960606e5adffd6a3308
sangrila22 <43953479+sangrila22@users.noreply.github.com>
2018-10-18 16:16:53 +0300

    Add files via upload

---
+++ Irma1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# create an original "Hello World" style message
+print("Hello World")
+
+
+# In[3]:
+
+
+# edit cell: add print() with the message "after edit, save!
+print("after edit, save!") 
+
+
+# In[5]:
+
+
+#edit cell: add print() with the message showing the keyboard Shortcut to save Ctrl + s
+print("Ctrl + s")
+
