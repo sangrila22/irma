dc5140558a3572ba57a7580af08b14516dc2d9bb
sangrila22 <43953479+sangrila22@users.noreply.github.com>
2020-01-09 13:41:30 +0200

    Add files via upload

---
+++ vism.js
@@ -0,0 +1,9 @@
+"use strict";
+
+
+console.log("JS vism");
+
+document.querySelector('.gallery0').addEventListener('click', function(){
+    document.querySelector('.gallery3').classList.toggle('show')
+
+});
